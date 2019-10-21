b504025fc2342b5c3726181ce02293c03e37c8a8
Thomas Crowley <crowley@kibu.com.au>
2019-10-21 20:30:37 +1100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "WebMIDIKit",
+    platforms: [
+        .macOS(.v10_13), .iOS(.v12),
+    ],
     products: [
         .library(
             name: "WebMIDIKit",
